e17c6fdc4d6a027bfc1ef653b9697d1ee564cfb0
Jayachandra Naidu <jayachandra.kunapareddy@emirates.com>
2018-08-02 16:51:58 +0400

    few changes to error handling

--- src/test/java/com/tests/okhttp/RestServiceTest.java
+++ src/test/java/com/tests/okhttp/RestServiceTest.java
@@ -3,7 +3,10 @@ package com.tests.okhttp;
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
 import okhttp3.Response;
+import org.junit.Assert;
 import org.junit.Test;
+
+import java.io.IOException;
 
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.MatcherAssert.assertThat;
@@ -11,7 +14,7 @@ import static org.hamcrest.MatcherAssert.assertThat;
 public class RestServiceTest {
 
     private final OkHttpClient client = new OkHttpClient();
-    private static final String URL = "https://jsonplaceholder.typicode.com/todos/1";
+    private static final String URL = "https://jsonplaceholderwertyuioiuytrewertyuik.typicode.com/todos/1";
 
     @Test
     public void serviceGetTest() {
@@ -20,13 +23,15 @@ public class RestServiceTest {
                 .header("Content-Type", "application/json")
                 .build();
         Response response = null;
+        String str = null;
         try {
             response = client.newCall(request).execute();
-            String str = response.body().string();
+            str = response.body().string();
             assertThat(response.code(), is(200));
             System.out.println(str);
-        } catch (Exception e) {
-            throw new RuntimeException("Unable to connect to server due to: " + e.getMessage());
+        } catch (IOException e) {
+            e.printStackTrace();
+            Assert.fail();
         }
     }
 }
